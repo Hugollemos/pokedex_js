0535cebb16f81abb2bed6635b86bcd4a41bbf979
hugo lemos <hugolemosdasilva.hu@gmail.com>
2021-10-18 14:01:51 -0300

    modificações

--- app.js
+++ app.js
@@ -1,32 +1,29 @@
-const fetchPokemon = () => {
-  const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
-  
-  const pokemonPromises = []
+const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
 
-  for (let i = 1; i <= 150; i++) {
-    pokemonPromises.push(fetch(getPokemonUrl(i)).then(response => response.json()))
-  }
+const generatePokemonPromises = () => Array(150).fill().map((_, index) =>
+  fetch(getPokemonUrl(index + 1)).then(response => response.json()))
+
+const generateHTML = pokemons => pokemons.reduce((accumulator, { name, id, types }) => {
+  const elemetTypes = types.map(typeInfo => typeInfo.type.name)
+
+  accumulator += `
+    <li class="card ${elemetTypes[0]}">
+    <img class="card-image" alt="${name}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png"/>
+      <h2 class="card-title>${id}. ${name}</h2>
+      <p class="card-subtitle">${elemetTypes.join(' | ')}</p>
+    </li>
+    `
+  return accumulator
+}, '')
 
 
-  Promise.all(pokemonPromises)
-    .then(pokemons => {
-      const lisPokemons = pokemons.reduce((accumulator, pokemon) => {
-        const types = pokemon.types.map(typeInfo => typeInfo.type.name)
+const insertPokemonsIntoPage = pokemons => {
+  const ul = document.querySelector('[data-js="pokedex"]')
+  ul.innerHTML = pokemons
+}
 
-        accumulator += `
-        <li class="card ${types[0]}">
-          <img class="card-image" alt="${pokemon.name}" src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png"/>
-          <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
-          <p class="card-subtitle">${types.join(' | ')}</p>
-        </li>
-        `
-        return accumulator
-      }, '')
+const PokemonPromises = generatePokemonPromises()
 
-      const ul = document.querySelector('[data-js="pokedex"]')
-
-      ul.innerHTML = lisPokemons
-    })
-} 
-
-fetchPokemon()
+Promise.all(PokemonPromises)
+  .then(generateHTML)
+  .then(insertPokemonsIntoPage)
